fbba6c83d042a8ef5c04497e3d513fa003681787
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-10-28 14:08:41 +0100

    feat: Add urlRequest.js

---
+++ public/js/urlRequest.js
@@ -0,0 +1,13 @@
+let form = document.querySelector("#urlInput");
+let urlinput = form.querySelector("input[name=url]");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  urlRequest();
+});
+
+function urlRequest() {
+  let url = urlinput.value.length == 0 ? urlinput.placeholder : urlinput.value;
+  console.log(url);
+  // request shortened url from the backend
+}
