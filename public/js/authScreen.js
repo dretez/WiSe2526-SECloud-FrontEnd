aafd4b090f960fc1d6d3dda07b9f8b2879cc7ddc
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-11-01 17:00:40 +0100

    feat: Add auth method selection

--- public/js/authScreen.js
+++ public/js/authScreen.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var selectedAuthOpt;
+
 let authscreen = document.querySelector("#authScreen");
 
 authscreen.querySelectorAll(".closePane").forEach((e) => {
@@ -13,3 +15,32 @@ document.querySelectorAll(".openAuthScreen").forEach((e) => {
     authscreen.classList.add("display");
   });
 });
+
+const authOpts = {
+  login: Array.from(
+    authscreen.querySelectorAll(".authMethodSelector > .authLogin"),
+  ),
+  register: Array.from(
+    authscreen.querySelectorAll(".authMethodSelector > .authRegister"),
+  ),
+};
+
+setAuthOpt("login");
+
+for (const [key, val] of Object.entries(authOpts)) {
+  val.forEach((e) => e.addEventListener("click", () => setAuthOpt(key)));
+}
+
+const authEnterBtn = authscreen.querySelector("input[name=authenter]");
+authscreen.querySelector("#loginForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+/*================================ FUNCTIONS ================================*/
+
+function setAuthOpt(opt) {
+  selectedAuthOpt = opt;
+  for (const [key, val] of Object.entries(authOpts)) {
+    val.forEach((e) => e.classList[key === opt ? "add" : "remove"]("selected"));
+  }
+}
