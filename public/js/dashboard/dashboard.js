003ebf01de4210c1522162233ae5eb40b0c31d17
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-11-05 22:01:34 +0100

    feat: Add button to open dashboard

--- public/js/dashboard/dashboard.js
+++ public/js/dashboard/dashboard.js
@@ -4,8 +4,15 @@ let api;
 
 import { LinkComponent } from "./linkComponent.js";
 
+const openButtons = document.querySelectorAll(".openDashboard");
 const dashboard = document.querySelector("#my-dashboard");
 const listDisplay = dashboard.querySelector("ul.link-list");
+
+openButtons.forEach((e) =>
+  e.addEventListener("click", () => {
+    dashboard.classList.add("display");
+  }),
+);
 
 let arr = [
   new LinkComponent(123, "https://one.one.one.one/", 23, "-", "-", false),
